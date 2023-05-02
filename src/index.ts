1b09d63bd7c5affd3c0f9c129a1357a10f97368b
JoaoRueda <joaomarcosrueda@gmail.com>
2023-05-01 23:37:32 -0300

    aula2 - para quando subscribe recebe error ou complete

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,36 @@
-import {from } from "rxjs";
+import {Observable, from } from "rxjs";
 
 let numbers = [1, 5, 10];
 let source = from(numbers);
 
 
-class myObserver {
-    next(x: number) {
-        console.log(x)
-    }
-    error(e: Error) {
-        console.log(e)        
-    }
-    complete(){
-        console.log('Complete')
-    }
-}
+/*
+tanto o complete quanto o error param imediatamente a chamada associada ao subscriber 
+*/
+let sourceInstance = new Observable(subscriber => {
+    for (let n of numbers) {
 
-const myObserverJson = {
-    next: (x: number) => console.log(x),
+        if (n > 5){
+            subscriber.error('Aconteceu um erro esperado!');
+        }
+
+        subscriber.next(n);
+        subscriber.complete();
+    }
+    subscriber.complete();
+})
+
+const myObserver = {
+    next: (x: number) => {
+        console.log(x)
+    },
     error: (e: Error) => console.log(e),
     complete: () => console.log('Complete'),
 }
 
 function component() {
-    // source.subscribe({
-    //     next: (x) => console.log(x)
-    // })
+    source.subscribe(myObserver)
+    sourceInstance.subscribe(myObserver)
+}
 
-    source.subscribe(new myObserver)
-  }
-
-  component();
+component();
